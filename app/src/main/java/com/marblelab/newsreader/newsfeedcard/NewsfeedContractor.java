6d85bfac424f249b56152f15c2f8584d59a1f6f8
Abid Hasan <abid.northsouth@gmail.com>
2017-05-10 23:59:12 +0600

    Interface Implementation

--- app/src/main/java/com/marblelab/newsreader/newsfeedcard/NewsfeedContractor.java
+++ app/src/main/java/com/marblelab/newsreader/newsfeedcard/NewsfeedContractor.java
@@ -1,8 +1,16 @@
 package com.marblelab.newsreader.newsfeedcard;
 
+import java.util.List;
+
 /**
  * Created by Abid Hasan on 5/10/2017.
  */
 
 public interface NewsfeedContractor {
+
+    public void parseData();
+    public List getData();
+    public void addToList();
+    public void checkForUpdate();
+    public void publish();
 }
--- app/src/main/java/com/marblelab/newsreader/newsfeedcard/NewsfeedPresenter.java
+++ app/src/main/java/com/marblelab/newsreader/newsfeedcard/NewsfeedPresenter.java
@@ -1,8 +1,36 @@
 package com.marblelab.newsreader.newsfeedcard;
 
+import java.util.List;
+
 /**
  * Created by Abid Hasan on 5/10/2017.
  */
 
-public class NewsfeedPresenter {
+public class NewsfeedPresenter implements NewsfeedContractor  {
+
+
+    @Override
+    public void parseData() {
+
+    }
+
+    @Override
+    public List getData() {
+        return null;
+    }
+
+    @Override
+    public void addToList() {
+
+    }
+
+    @Override
+    public void checkForUpdate() {
+
+    }
+
+    @Override
+    public void publish() {
+
+    }
 }
